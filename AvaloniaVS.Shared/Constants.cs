8ef0792030d2f09b26b21d3cfb80edd117b979a9
Damian Suess <djsuess@gmail.com>
2025-10-22 10:41:31 -0400

    Legacy branding

--- AvaloniaVS.Shared/Constants.cs
+++ AvaloniaVS.Shared/Constants.cs
@@ -6,7 +6,7 @@ internal static class Constants
 {
     public const string PackageGuidString = "57F96315-438D-4BB2-9D03-F2527301A1D3"; // "865ba8d5-1180-4bf8-8821-345f72a4cb79";
     public static readonly Guid PackageGuid = new (PackageGuidString);
-    public const string PackageName = "Avalonia-Lite Xaml Editor";
+    public const string PackageName = "Avalonia Legacy Xaml Editor";
     public const string axaml = nameof(axaml);
     public const string xaml = nameof(xaml);
     public const string paml = nameof(paml);
