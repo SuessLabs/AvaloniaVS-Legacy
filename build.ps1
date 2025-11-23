e09577e271ecf745ff089a9060ce106ebada8052
Damian Suess <djsuess@gmail.com>
2025-11-23 14:55:11 -0500

    Updated build script to send binaries to `output` and `publish` folders. Cleaned up readme notes

--- build.ps1
+++ build.ps1
@@ -1,9 +1,19 @@
 # Build script for generating release package
 
+Write-Output "Cleaning output folders..."
+
 if (Test-Path -Path "AvaloniaVS.VS2022\bin")
 {
   Remove-Item AvaloniaVS.VS2022\bin\* -Recurse -Force
 }
+
+if (Test-Path -Path "output\")
+{
+  Remove-Item output\* -Recurse -Force
+}
+
+# Clean both debug and release
+dotnet clean AvaloniaVSExt.slnx --configuration Release
 
 $VCToolsInstallDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -Latest -requires Microsoft.Component.MSBuild -property InstallationPath
 Write-Host "VCToolsInstallDir: $VCToolsInstallDir"
@@ -18,3 +28,17 @@ Write-Host "Building..."
 & $msBuildPath /restore `
                AvaloniaVSExt.slnx `
                /p:Configuration=Release
+
+# Publish
+Write-Output "Cleaning Publish folder.."
+
+if (Test-Path -Path "publish\")
+{
+  Remove-Item publish\* -Recurse -Force
+}
+else
+{
+  New-Item -Path '.\publish' -ItemType Directory
+}
+
+Move-Item -Path .\output\Release\AvaloniaVS.VS2022.vsix -Destination .\publish\AvaloniaVS.VS2022.vsix
